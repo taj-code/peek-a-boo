a31fe78d37da62c830e311ae06d8b8699165d6e1
taj-code <tajofisher4@gmail.com>
2021-08-10 10:22:42 -0700

    Update css and add more rules

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,29 @@ $(document).ready(function() {
     $(".walrus-showing").show();
   });
 });
+
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".walrus-showing").show();
+    $(".walrus-hidden").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+});
